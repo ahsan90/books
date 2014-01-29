9c14e615f7fcbfe2ef2c41bfff2658cfb5496a12
Ladydanger <avj.nguyen@gmail.com>
2014-01-30 09:16:05 +1100

    Created welcome controller with index and contact views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Books::Application.routes.draw do
+  get "welcome/index"
+
+  get "welcome/contact"
+
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
