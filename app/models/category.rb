1b38a173b5e0eb51d2f8c2d2204e788f1214b3db
Ladydanger <avj.nguyen@gmail.com>
2014-02-05 15:35:05 +1100

    Debug confirmations for user

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
   attr_accessible :body, :title
   has_many :books
+  #def dropdown
+  # Category.All
+
+    #category.find_or_create_by(Category.All)
+  #end
 end
